b811736ea4ebf6790d9da4440bcb74828e5d2fbf
twinemarron <twinemarron@gmail.com>
2021-02-12 00:27:24 +0900

    after section 2-9

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -3,6 +3,7 @@ package users
 import (
 	"fmt"
 
+	"github.com/twinemarron/bookstore_users-api/utils/date_utils"
 	"github.com/twinemarron/bookstore_users-api/utils/errors"
 )
 
@@ -41,6 +42,9 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d aleady exists", user.Id))
 	}
+
+	user.DateCreated = date_utils.GetNowString()
+
 	usersDB[user.Id] = user
 	return nil
 }
