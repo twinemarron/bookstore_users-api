e2ae8a744234dc512a11df7547bd3a86883600a5
twinemarron <twinemarron@gmail.com>
2021-02-14 23:20:11 +0900

    after section 3-11

---
+++ sql/create_table_users_db_users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users_db`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `date_created` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
